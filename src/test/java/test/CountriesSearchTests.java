7fa6e23046429787de6cc4fb896d112e99161835
Rohit Rawat <rrawat@c02v35bvhtd6.myfamily.int>
2018-04-06 12:25:08 -0600

    [Adding files for project ]

---
+++ src/test/java/test/CountriesSearchTests.java
@@ -0,0 +1,63 @@
+package test;
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import net.serenitybdd.junit.runners.SerenityRunner;
+
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SerenityRunner.class)
+
+public class CountriesSearchTests {
+
+	
+	@Test
+	public void verifyThatWeCanFindUnitedStatesOfAmericaUsingTheCodeUS(){
+		RestAssured.
+		when().get("http://services.groupkt.com/country/get/iso2code/US").
+		then().assertThat().statusCode(200).
+		and().body("RestResponse.result.name", is("United States of America"));
+	}
+
+	@Test
+	public void verifyThatWeCanFindIndiaCountryUsingTheCodeIN(){
+		RestAssured.
+		when().get("http://services.groupkt.com/country/get/iso2code/IN").
+		then().assertThat().statusCode(200).
+		and().body("RestResponse.result.name", is("India"));
+	}
+
+	@Test
+	public void verifyThatWeCanFindBrazilCountryUsingTheCodeBR(){
+		RestAssured.
+		when().get("http://services.groupkt.com/country/get/iso2code/BR").
+		then().assertThat().statusCode(200).
+		and().body("RestResponse.result.name", is("Brazil"));
+	}
+	@Test
+	public void verifyThatWeCanFindUnitedStatesOfAmericaUsingTheCodeUS1(){
+		RestAssured.
+		when().get("http://services.groupkt.com/country/get/iso2code/US").
+		then().assertThat().statusCode(200).
+		and().body("RestResponse.result.name", is("United States of America"));
+	}
+
+	@Test
+	public void verifyThatWeCanFindIndiaCountryUsingTheCodeIN1(){
+		RestAssured.
+		when().get("http://services.groupkt.com/country/get/iso2code/IN").
+		then().assertThat().statusCode(200).
+		and().body("RestResponse.result.name", is("India"));
+	}
+
+	@Test
+	public void verifyThatWeCanFindBrazilCountryUsingTheCodeBR1(){
+		RestAssured.
+		when().get("http://services.groupkt.com/country/get/iso2code/BR").
+		then().assertThat().statusCode(200).
+		and().body("RestResponse.result.name", is("Brazil"));
+	}
+
+
+}
